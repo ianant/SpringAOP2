d830d7a2fa4a9af36fa0426d1afb69f13a9f53e2
Anant <Anant@LAPTOP-AC1KTRDJ>
2020-01-26 22:01:50 +0530

    @AfterThrowing Advise changes: Created new main demo app for after throwing advise

---
+++ Spring-AOP-Z-After-Returning/src/com/anant/spring/aop/AOPZAfterThrowingDemoApp.java
@@ -0,0 +1,31 @@
+package com.anant.spring.aop;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.anant.spring.aop.dao.AccountDAO;
+
+public class AOPZAfterThrowingDemoApp {
+
+	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(AOPDemo.class);
+		
+		AccountDAO theDao=context.getBean("accountDAO",AccountDAO.class);
+		
+		List<Account> theAccounts=null;
+		
+		try {
+			theAccounts=theDao.listAccounts(true);
+		}
+		catch(Exception exc){
+			System.out.println("\nExecptionOccured for after throwing");
+		}
+		
+		System.out.println("\n\nMain demo app account list: "+theAccounts);
+		context.close();
+
+	}
+
+}
